03621a2b3fd1af0834f97fec713426030a8ae0b6
emadunan <emadunan@gmail.com>
2025-01-24 13:14:36 +0200

    feat: Add and configure i18n namespaces for different languages

--- mobile/i18n/i18n.ts
+++ mobile/i18n/i18n.ts
@@ -2,15 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
 
-import translationEn from "./locales/en.json";
-import translationAr from "./locales/ar.json";
+import commonEn from "./locales/en/common.json";
+import commonAr from "./locales/ar/common.json";
 
 const resources = {
   en: {
-    translation: translationEn,
+    common: commonEn,
   },
   ar: {
-    translation: translationAr,
+    common: commonAr,
   },
 };
 
@@ -20,6 +20,8 @@ i18n.use(initReactI18next).init<Resources>({
   resources,
   fallbackLng: "en",
   lng: Localization.getLocales()[0].languageCode ?? "en",
+  defaultNS: 'common',
+  ns: ['common']
 });
 
 export default i18n;
