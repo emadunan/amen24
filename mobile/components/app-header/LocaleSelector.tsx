146cd4c6c011f5920e7ff8b04a9555b89a81df75
emadunan <emadunan@gmail.com>
2025-01-26 09:16:05 +0200

    feat: Use react-native-restart to restart the app after force RTL

--- mobile/components/app-header/LocaleSelector.tsx
+++ mobile/components/app-header/LocaleSelector.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Pressable } from 'react-native'
-import { IconSymbol } from '../ui/IconSymbol'
-import i18n from '@/i18n/i18n'
+import React from 'react';
+import { I18nManager, Platform, Pressable } from 'react-native';
+import { IconSymbol } from '../ui/IconSymbol';
+import i18n from '@/i18n/i18n';
+import RNRestart from "react-native-restart";
+import * as SplashScreen from 'expo-splash-screen';
+
 
 const LocaleSelector = () => {
 
-  function handleLocale() {
-    const newLanguage = i18n.language === 'en' ? 'ar' : 'en';
+  async function handleLocale() {
+    await SplashScreen.preventAutoHideAsync();
 
+    const newLanguage = i18n.language === 'en' ? 'ar' : 'en';
     i18n.changeLanguage(newLanguage);
+
+    const isRtl = newLanguage === "ar";
+
+    // if (isRtl !== I18nManager.isRTL && Platform.OS !== 'web') {}
+    I18nManager.allowRTL(isRtl);
+    I18nManager.forceRTL(isRtl);
+
+    RNRestart.restart();
   }
 
   return (
