d9cd7fd70b759dfab35469ea93eaec9ced930f5f
emadunan <emadunan@gmail.com>
2025-01-26 11:03:54 +0200

    chore: Reconfigure expo-updates in app.json

--- mobile/components/app-header/LocaleSelector.tsx
+++ mobile/components/app-header/LocaleSelector.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { I18nManager, Platform, Pressable } from 'react-native';
 import { IconSymbol } from '../ui/IconSymbol';
 import i18n from '@/i18n/i18n';
-import RNRestart from "react-native-restart";
-import * as SplashScreen from 'expo-splash-screen';
+import * as Updates from "expo-updates";
 
 
 const LocaleSelector = () => {
 
-  async function handleLocale() {
-    await SplashScreen.preventAutoHideAsync();
+  function handleLocale() {
 
     const newLanguage = i18n.language === 'en' ? 'ar' : 'en';
     i18n.changeLanguage(newLanguage);
@@ -19,8 +17,7 @@ const LocaleSelector = () => {
     // if (isRtl !== I18nManager.isRTL && Platform.OS !== 'web') {}
     I18nManager.allowRTL(isRtl);
     I18nManager.forceRTL(isRtl);
-
-    RNRestart.restart();
+    Updates.reloadAsync();
   }
 
   return (
