7d06885c7492564dc27abb69ce5493c216c9b9a7
emadunan <emadunan@gmail.com>
2025-02-01 17:05:07 +0200

    feat: Show every verse in s separate line

--- mobile/components/bible/BibleChapterText.tsx
+++ mobile/components/bible/BibleChapterText.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useEffect, useState } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 import { ThemedText } from "../ThemedText";
-import { convertToSuperscript } from "@/utils";
 import { useSQLiteContext } from "expo-sqlite";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { useTranslation } from "react-i18next";
+import { ThemedView } from "../ThemedView";
 
 type BibleLang = "En" | "Ar";
 
@@ -27,11 +27,8 @@ const BibleChapterText: FC<Props> = ({
   setHighlighted,
 }) => {
   const db = useSQLiteContext();
-
   const { i18n } = useTranslation();
-
   const [verses, setVerses] = useState<{ num: number; text: string }[]>([]);
-
   const colorScheme = useColorScheme();
 
   const highlightTheme = {
@@ -41,8 +38,11 @@ const BibleChapterText: FC<Props> = ({
   useEffect(() => {
     const fetchChapter = async () => {
       const data = await db.getAllAsync<{ num: number; text: string }>(
-        `SELECT verses${bibleLang}.num, text FROM verses${bibleLang} LEFT JOIN chapters ON verses${bibleLang}.chapterId = chapters.id LEFT JOIN books ON chapters.bookId = books.id WHERE chapters.num = ? AND books.id = ?;`,
-        [chapterNum, bookId],
+        `SELECT verses${bibleLang}.num, text FROM verses${bibleLang} 
+        LEFT JOIN chapters ON verses${bibleLang}.chapterId = chapters.id 
+        LEFT JOIN books ON chapters.bookId = books.id 
+        WHERE chapters.num = ? AND books.id = ?;`,
+        [chapterNum, bookId]
       );
 
       setVerses(data);
@@ -54,31 +54,36 @@ const BibleChapterText: FC<Props> = ({
 
   function handleHighlight(verseNum: number) {
     const num = verseNum.toString();
-
     setHighlighted((prevState) =>
       prevState.includes(num)
         ? prevState.filter((v) => v !== num)
-        : [...prevState, num],
+        : [...prevState, num]
     );
   }
 
   return (
-    <Text style={styles.chapterText}>
-      {verses.map((v) => (
-        <ThemedText
-          key={v.num}
-          style={[
-            styles.verseText,
-            highlighted.includes(v.num.toString()) && highlightTheme,
-          ]}
-        >
-          <Pressable onPress={handleHighlight.bind(this, v.num)}>
-            <Text style={styles.verseNum}>{i18n.language === "ar" ? v.num.toLocaleString("ar-EG") : v.num}</Text>
+    <ThemedView>
+      <Text style={styles.chapterText}>
+        {verses.map((v, index) => (
+          <Pressable key={v.num} onPress={() => handleHighlight(v.num)}>
+            <Text
+              style={[
+                styles.verseText,
+                highlighted.includes(v.num.toString()) && highlightTheme,
+              ]}
+            >
+              <Text style={styles.verseNum}>
+                {i18n.language === "ar"
+                  ? v.num.toLocaleString("ar-EG") // Arabic numerals
+                  : v.num}
+              </Text>{" "}
+              {v.text}
+              {index < verses.length - 1 ? " " : ""} {/* Space between verses */}
+            </Text>
           </Pressable>
-          {v.text}{" "}
-        </ThemedText>
-      ))}
-    </Text>
+        ))}
+      </Text>
+    </ThemedView>
   );
 };
 
@@ -86,15 +91,17 @@ export default BibleChapterText;
 
 const styles = StyleSheet.create({
   chapterText: {
-    // textAlign: "justify"
+    fontSize: 18,
+    lineHeight: 28,
+    textAlign: "justify",
   },
   verseText: {
     fontSize: 18,
     lineHeight: 28,
-    alignItems: "flex-start",
   },
   verseNum: {
-    color: "#f00",
     fontWeight: "bold",
+    fontSize: 16,
+    color: "#f00",
   },
 });
