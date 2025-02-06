86fa83bc383c02a3706f945f25042acb4632822d
emadunan <emadunan@gmail.com>
2025-02-06 22:08:25 +0200

    refactor: Use router.replace instead of router.push to locale navigation, and remove unused changeLocal function

--- mobile/components/app-header/LocaleSelector.tsx
+++ mobile/components/app-header/LocaleSelector.tsx
@@ -1,40 +1,14 @@
 import React from "react";
-import { I18nManager, Pressable } from "react-native";
-import i18n from "@/i18n/i18n";
-import * as Updates from "expo-updates";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Pressable } from "react-native";
 import { Colors } from "@/constants/Colors";
 import { useRouter } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const LocaleSelector = () => {
   const router = useRouter();
 
-  async function handleLocale() {
-    const newLanguage = i18n.language === "en" ? "ar" : "en";
-    i18n.changeLanguage(newLanguage);
-
-    const isRtl = newLanguage === "ar";
-
-    // if (isRtl !== I18nManager.isRTL && Platform.OS !== 'web') {}
-    I18nManager.allowRTL(isRtl);
-    I18nManager.forceRTL(isRtl);
-
-    if (!__DEV__) {
-      // Only run in production
-      try {
-        await Updates.reloadAsync();
-      } catch (e) {
-        console.error("Failed to reload app: ", e);
-      }
-    } else {
-      console.warn(
-        "Updates.reloadAsync() does not work in Expo Go or development mode.",
-      );
-    }
-  }
-
   function handleRedirect() {
-    router.push("/locale");
+    router.replace("/locale");
   }
 
   return (
