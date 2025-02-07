76795439e7e13994f9a7f35c4a1ff797e0990128
emadunan <emadunan@gmail.com>
2025-02-07 15:28:25 +0200

    chore: Install nestjs/passport, and configure dataSource to migrate user entity

---
+++ backend/_config/dataSource.config.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
+import { DataSource, type DataSourceOptions } from 'typeorm';
+
+const currentEnv = process.env.NODE_ENV;
+const path = resolve(__dirname, '..', '..', `.env.${currentEnv}`);
+
+config({ path });
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  migrations: ['**/migrations/*.ts'],
+  entities: ['**/*.entity.ts'],
+  subscribers: ['**/*.subscriber.ts'],
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
--- backend/env.d.ts
+++ backend/env.d.ts
@@ -1,11 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
+    PORT: string;
+    HOST: string;
+    DB_NAME: string;
     DB_HOST: string;
     DB_PORT: string;
-    DB_USER: string;
-    DB_PASS: string;
-    DB_NAME: string;
-    PORT: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
   }
 }
