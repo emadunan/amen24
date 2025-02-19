3d929e52edb7e8efb8f3f1e69e4370eb14dc2985
emadunan <emadunan@gmail.com>
2025-02-20 00:42:18 +0200

    chore: Reconfigure project to be managed by npm, and install react-icons

--- frontend/src/components/layout/AppHeader.tsx
+++ frontend/src/components/layout/AppHeader.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from "./AppHeader.module.css";
+import Link from 'next/link';
+
 
 const AppHeader = () => {
   return (
     <header className={styles.appHeader}>
-      <Image src="/amen-24.png" alt='Amen24 logo' width={124} height={124}/>
+      <div className={styles.container}>
+        <h1 className={styles.appLogo}>Amen 24</h1>
+        <nav>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}><Link href={"/"}> <h3>Bible</h3></Link></li>
+            <li className={styles.navItem}><Link href={"/search"}><h3>Search</h3></Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
