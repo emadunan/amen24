c9da4e0e4063bb0a428d8cf3d9f1d4febaf6fb00
emadunan <emadunan@gmail.com>
2025-03-28 19:57:28 +0200

    refactore: Prefix all backend api endpoints with /api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
