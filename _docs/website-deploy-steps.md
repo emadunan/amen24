5ccbde132eaddf5ffcb58ccf9a40fc11e9414146
emadunan <emadunan@gmail.com>
2025-03-28 19:37:18 +0200

    docs: Add setup-test bash script to setup testing environment

---
+++ _docs/website-deploy-steps.md
@@ -0,0 +1,11 @@
+Amen24 website production environment setup
+
+- Create a linux machine in hosting provider
+- Login with root and create sudo user
+- Add sshkey for the sudo user
+- Clone the project into projects directory in the home of the sudo user
+- Install postgresql
+
+- Install nodejs
+- Install nestjs and pm2 globally
+- Install nginx from ubuntu repository
---
+++ _scripts/setup-test.sh
@@ -0,0 +1,22 @@
+# Install shared dependencies and build
+cd ../shared
+npm i
+npm run build
+
+# Setup backend api 
+cd ../backend
+npm i
+npm run build
+npm run migrate:up:test
+npm run seed:test
+cd ..
+pm2 start ecosystem.config.js --only backend --env test
+
+# Setup frontend website
+cd ./frontend
+npm i
+npm run build:test
+cd ..
+pm2 start ecosystem.config.js --only frontend --env test
+
+# 
