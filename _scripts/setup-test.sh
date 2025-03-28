012274fedd2f9bef7a30da0fa3af879f81a664f9
emadunan <emadunan@gmail.com>
2025-03-28 20:53:26 +0200

    test: Allow cors for all origins temporarly

--- _scripts/setup-test.sh
+++ _scripts/setup-test.sh
@@ -6,6 +6,7 @@ npm run build
 # Setup backend api 
 cd ../backend
 npm i
+rm -Rf dist
 npm run build
 npm run migrate:up:test
 npm run seed:test
@@ -15,6 +16,7 @@ pm2 start ecosystem.config.js --only backend --env test
 # Setup frontend website
 cd ./frontend
 npm i
+rm -Rf .next
 npm run build:test
 cd ..
 pm2 start ecosystem.config.js --only frontend --env test
