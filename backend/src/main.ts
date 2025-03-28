6e08d1c294e328559b7747a7706c4630878912a4
emadunan <emadunan@gmail.com>
2025-03-28 21:18:12 +0200

    fix: Replace * wildcard with FRONTEND_ORIGINS from dotenv

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     : [];
 
   app.enableCors({
-    origin: "*",
+    origin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
