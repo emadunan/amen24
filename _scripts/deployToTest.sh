5dea342c04a999b8957970b2d43739ce8c3c2986
emadunan <emadunan@gmail.com>
2025-03-29 16:37:17 +0200

    chore: Add workflow for automation pipeline demo

---
+++ _scripts/deployToTest.sh
@@ -0,0 +1,27 @@
+# Delete current deploy
+pm2 del ecosystem.config.js 
+
+# Install shared dependencies and build
+cd ../shared
+npm i
+npm run build
+
+# Setup backend api 
+cd ../backend
+npm i
+rm -Rf dist
+npm run build
+npm run migrate:up:test
+cd ..
+pm2 start ecosystem.config.js --only backend --env test
+
+# Setup frontend website
+cd ./frontend
+npm i
+rm -Rf .next
+npm run build:test
+cd ..
+pm2 start ecosystem.config.js --only frontend --env test
+
+# Restart nginx
+sudo systemctl restart postgresql.service
