ab279d0b7af52c0e10df33b623cf2ce38c393cb0
emadunan <emadunan@gmail.com>
2025-03-31 14:07:05 +0200

    fix: Restore Hebrew date in the calendar to satisfy eslint

--- frontend/src/components/ui/DateDisplay.tsx
+++ frontend/src/components/ui/DateDisplay.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import styles from "./DateDisplay.module.css";
 import { formatNumber, Lang } from "@amen24/shared";
 
-const calendars = ["gregorian", "coptic"] as const;
+const calendars = ["gregorian", "coptic", "hebrew"] as const;
 type CalendarType = (typeof calendars)[number];
 
 const getLocaleForCalendar = (calendar: CalendarType) => {
@@ -12,28 +12,28 @@ const getLocaleForCalendar = (calendar: CalendarType) => {
       return "en"; // Default to avoid errors
     case "coptic":
       return "en-u-ca-coptic";
-    // case "hebrew":
-    //   return "en-u-ca-hebrew";
+    case "hebrew":
+      return "en-u-ca-hebrew";
     default:
       return "en"; // Fallback
   }
 };
 
-// const HebrewMonthMap: Record<string, number> = {
-//   Nisan: 1,
-//   Iyyar: 2,
-//   Sivan: 3,
-//   Tammuz: 4,
-//   Av: 5,
-//   Elul: 6,
-//   Tishrei: 7,
-//   Cheshvan: 8,
-//   Kislev: 9,
-//   Tevet: 10,
-//   Shevat: 11,
-//   Adar: 12,
-//   "Adar II": 13, // Only in leap years
-// };
+const HebrewMonthMap: Record<string, number> = {
+  Nisan: 1,
+  Iyyar: 2,
+  Sivan: 3,
+  Tammuz: 4,
+  Av: 5,
+  Elul: 6,
+  Tishrei: 7,
+  Cheshvan: 8,
+  Kislev: 9,
+  Tevet: 10,
+  Shevat: 11,
+  Adar: 12,
+  "Adar II": 13, // Only in leap years
+};
 
 const DateDisplay: React.FC = () => {
   const { t, i18n } = useTranslation("month");
@@ -61,16 +61,16 @@ const DateDisplay: React.FC = () => {
 
     if (!day || !monthIndex || !year) return "Error fetching date";
 
-    // if (locale === "en-u-ca-hebrew") {
-    //   // Hebrew months are returned as names, not numbers
-    //   const mappedMonth = HebrewMonthMap[monthIndex]; // Convert name to number
-    //   if (mappedMonth) {
-    //     monthIndex = mappedMonth.toString();
-    //   } else {
-    //     console.warn(`Unexpected Hebrew month: ${monthIndex}`); // Debugging help
-    //     return "Invalid Hebrew date"; // Avoid crashing if mapping fails
-    //   }
-    // }
+    if (locale === "en-u-ca-hebrew") {
+      // Hebrew months are returned as names, not numbers
+      const mappedMonth = HebrewMonthMap[monthIndex]; // Convert name to number
+      if (mappedMonth) {
+        monthIndex = mappedMonth.toString();
+      } else {
+        console.warn(`Unexpected Hebrew month: ${monthIndex}`); // Debugging help
+        return "Invalid Hebrew date"; // Avoid crashing if mapping fails
+      }
+    }
 
     // Get translated month name
     const monthName = t(`${calendar}.${monthIndex}`, { ns: "month" });
