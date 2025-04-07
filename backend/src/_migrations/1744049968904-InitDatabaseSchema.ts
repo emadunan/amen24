4914b8d8b7d21ce698da5a89c6beca29b3415385
emadunan <emadunan@gmail.com>
2025-04-07 20:25:48 +0200

    refactor: Remove all old db migrations and produce start up new migration

--- backend/src/_migrations/1744049968904-InitDatabaseSchema.ts
+++ backend/src/_migrations/1744049968904-InitDatabaseSchema.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitDatabaseSchema1744029303745 implements MigrationInterface {
-    name = 'InitDatabaseSchema1744029303745'
+export class InitDatabaseSchema1744049968904 implements MigrationInterface {
+    name = 'InitDatabaseSchema1744049968904'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "book" ("id" SERIAL NOT NULL, "bookKey" character varying NOT NULL, CONSTRAINT "UQ_7a8de40b0468ea207173269b602" UNIQUE ("bookKey"), CONSTRAINT "PK_a3afef72ec8f80e6e5c310b28a4" PRIMARY KEY ("id"))`);
@@ -9,8 +9,8 @@ export class InitDatabaseSchema1744029303745 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" text NOT NULL, "password" text, "displayName" character varying NOT NULL, "provider" text NOT NULL DEFAULT 'local', "providerId" text, "photoUri" text, "isActive" boolean NOT NULL DEFAULT true, "isVerified" boolean NOT NULL DEFAULT false, "failedAttempts" smallint NOT NULL DEFAULT '0', "lockUntil" TIMESTAMP WITH TIME ZONE, "resetPasswordToken" text, "resetPasswordExpires" TIMESTAMP WITH TIME ZONE, CONSTRAINT "UQ_360717bb2dcd77e2d9e4e6e5259" UNIQUE ("email", "provider"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "bookmark" ("id" SERIAL NOT NULL, "title" text NOT NULL, "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "profileEmail" text NOT NULL, "verseId" integer, CONSTRAINT "UQ_dbbe441c08d730fe1a91ac17f16" UNIQUE ("profileEmail", "verseId", "title"), CONSTRAINT "PK_b7fbf4a865ba38a590bb9239814" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "profile" ("email" text NOT NULL, "privilege" text NOT NULL DEFAULT 'member', "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "lastLogin" TIMESTAMP WITH TIME ZONE DEFAULT now(), "uiLang" text, "fontSize" smallint NOT NULL DEFAULT '1', "themeMode" text NOT NULL DEFAULT 'light', "dateCalendar" text NOT NULL DEFAULT 'gregorian', "isDiacritized" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_3825121222d5c17741373d8ad13" PRIMARY KEY ("email"))`);
-        await queryRunner.query(`CREATE TABLE "favorite" ("id" SERIAL NOT NULL, "profileEmail" text NOT NULL, "verseGroupId" integer, CONSTRAINT "UQ_d0bd95dd49ce11856904f737fb9" UNIQUE ("profileEmail", "verseGroupId"), CONSTRAINT "PK_495675cec4fb09666704e4f610f" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "verse_group" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_ef2c6122160cd45ad4aa874900f" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "favorite" ("id" SERIAL NOT NULL, "profileEmail" text NOT NULL, "verseGroupId" integer NOT NULL, CONSTRAINT "UQ_d0bd95dd49ce11856904f737fb9" UNIQUE ("profileEmail", "verseGroupId"), CONSTRAINT "PK_495675cec4fb09666704e4f610f" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "verse_group" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "startingVerseId" integer, CONSTRAINT "PK_ef2c6122160cd45ad4aa874900f" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "verse_translation" ("id" SERIAL NOT NULL, "lang" text NOT NULL, "text" text NOT NULL, "textNormalized" text NOT NULL, "textDiacritized" text, "verseId" integer NOT NULL, CONSTRAINT "PK_c3b7119a1f36c274175162ff963" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_lang" ON "verse_translation" ("lang") `);
         await queryRunner.query(`CREATE TABLE "verse" ("id" SERIAL NOT NULL, "num" smallint NOT NULL, "chapterId" integer NOT NULL, CONSTRAINT "UQ_52e58631a3eea5d5b68a441fdbe" UNIQUE ("chapterId", "num"), CONSTRAINT "PK_1bf192a2ee81fa999f9a75939e4" PRIMARY KEY ("id"))`);
@@ -22,7 +22,8 @@ export class InitDatabaseSchema1744029303745 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "bookmark" ADD CONSTRAINT "FK_1d0085d0d2f724fa5fe0e1d39d1" FOREIGN KEY ("profileEmail") REFERENCES "profile"("email") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "bookmark" ADD CONSTRAINT "FK_4cd4002ed01cdb4660302d912f3" FOREIGN KEY ("verseId") REFERENCES "verse"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "favorite" ADD CONSTRAINT "FK_4ac9701e06098ad724c34f80a1d" FOREIGN KEY ("profileEmail") REFERENCES "profile"("email") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "favorite" ADD CONSTRAINT "FK_4280ca834f31e51ddc59343bb32" FOREIGN KEY ("verseGroupId") REFERENCES "verse_group"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "favorite" ADD CONSTRAINT "FK_4280ca834f31e51ddc59343bb32" FOREIGN KEY ("verseGroupId") REFERENCES "verse_group"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "verse_group" ADD CONSTRAINT "FK_02a82cc44ba88d7f19321f1530b" FOREIGN KEY ("startingVerseId") REFERENCES "verse"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "verse_translation" ADD CONSTRAINT "FK_e5078846f8fbbe8b533d48fce6c" FOREIGN KEY ("verseId") REFERENCES "verse"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "verse" ADD CONSTRAINT "FK_5784019838c4e9808ba55d81f82" FOREIGN KEY ("chapterId") REFERENCES "chapter"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "verse_group_verses" ADD CONSTRAINT "FK_ff4e636cb21d32506b31b34f46f" FOREIGN KEY ("verseGroupId") REFERENCES "verse_group"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
@@ -34,6 +35,7 @@ export class InitDatabaseSchema1744029303745 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "verse_group_verses" DROP CONSTRAINT "FK_ff4e636cb21d32506b31b34f46f"`);
         await queryRunner.query(`ALTER TABLE "verse" DROP CONSTRAINT "FK_5784019838c4e9808ba55d81f82"`);
         await queryRunner.query(`ALTER TABLE "verse_translation" DROP CONSTRAINT "FK_e5078846f8fbbe8b533d48fce6c"`);
+        await queryRunner.query(`ALTER TABLE "verse_group" DROP CONSTRAINT "FK_02a82cc44ba88d7f19321f1530b"`);
         await queryRunner.query(`ALTER TABLE "favorite" DROP CONSTRAINT "FK_4280ca834f31e51ddc59343bb32"`);
         await queryRunner.query(`ALTER TABLE "favorite" DROP CONSTRAINT "FK_4ac9701e06098ad724c34f80a1d"`);
         await queryRunner.query(`ALTER TABLE "bookmark" DROP CONSTRAINT "FK_4cd4002ed01cdb4660302d912f3"`);
