a12e7dae8f78b4572fd0e656816c262ac4082606
emadunan <emadunan@gmail.com>
2025-04-23 15:49:47 +0200

    refactor: Use createAuthApi from @amen24/ui and delete local implementation inside admin app

--- apps/admin/src/store/authApi.ts
+++ apps/admin/src/store/authApi.ts
@@ -1,32 +1,5 @@
-// Need to use the React-specific entry point to import createApi
-import { User } from '@amen24/shared';
-import { createApi } from '@reduxjs/toolkit/query/react';
-import { createBaseQueryWithReauth } from './baseQueryWithReauth';
+import { createAuthApi } from "@amen24/ui";
+import { apiUrl } from "../constants";
 
-type UserLogin = Pick<User, "email" | "password">;
-
-export const authApi = createApi({
-  reducerPath: 'authApi',
-  tagTypes: ["User"],
-  baseQuery: createBaseQueryWithReauth("auth"),
-  endpoints: (builder) => ({
-    getMe: builder.query<User | null, void>({
-      query: () => `/me`,
-      providesTags: ["User"],
-    }),
-    login: builder.mutation<void, UserLogin>({
-      query(body) {
-        return {
-          url: `/local`,
-          method: "POST",
-          body
-        }
-      },
-      invalidatesTags: ["User"]
-    })
-  }),
-})
-
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-export const { useGetMeQuery, useLoginMutation } = authApi
+export const authApi = createAuthApi(apiUrl);
+export const { useGetMeQuery, useLoginMutation } = authApi;
