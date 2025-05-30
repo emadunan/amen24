690adbc4d92a47e2445099da64bd84000daddc88
emadunan <emadunan@gmail.com>
2025-05-31 01:36:33 +0300

    refactor: Add photo for Jesus in Messiah section and rewrite paragraphs

--- apps/frontend/src/components/home/MessiahSection.tsx
+++ apps/frontend/src/components/home/MessiahSection.tsx
@@ -3,22 +3,34 @@
 import Link from "next/link";
 import styles from "./MessiahSection.module.css";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
 
 const MessiahSection = () => {
   const { t } = useTranslation();
 
   return (
     <section className={styles.container}>
-      <h2 className={styles.title}>{t("homepage.messiahSectionHeading")}</h2>
       <div className={styles.content}>
+        <h2 className={styles.title}>{t("homepage.messiahSectionHeading")}</h2>
         <p className={styles.paragraph}>
           {t("homepage.messiahSectionParagraph1")}
         </p>
         <p className={styles.paragraph}>
-        {t("homepage.messiahSectionParagraph2")}
+          {t("homepage.messiahSectionParagraph2")}
         </p>
+        <Link href={"/MAT/1/28"} className={styles.link}>{t("homepage.messiahSectionButton")}</Link>
       </div>
-      <Link href={"/LUK/1/24"} className={styles.link}>{t("homepage.messiahSectionButton")}</Link>
+      <div className={styles.imageContainer}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/img/jesus.jpg"
+            alt="Open Bible"
+            fill
+            className={styles.image}
+            priority
+          />
+        </div>
+      </div>
     </section>
   );
 };
