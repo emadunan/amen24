949bd9c42e4a084bd8cc76fc47231197eb08e642
emadunan <emadunan@gmail.com>
2025-07-04 12:16:18 +0300

    chore: Update the mobile app version to 1.1.0

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "Amen24",
     "slug": "amen24",
-    "version": "1.0.1",
+    "version": "1.1.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "amen24",
