23d23fdd8710d588fc209dd810d833c8c9d4b808
emadunan <emadunan@gmail.com>
2025-07-06 22:24:13 +0300

    chore: Handle version in eas automatically based on package.json

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,58 +1,59 @@
 import "dotenv/config";
+import pkg from "./package.json";
 
 export default {
-  "expo": {
-    "name": "Amen24",
-    "slug": "amen24",
-    "version": "1.1.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "amen24",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "updates": {
-      "enabled": true,
-      "checkAutomatically": "ON_LOAD",
-      "fallbackToCacheTimeout": 0
+  expo: {
+    name: "Amen24",
+    slug: "amen24",
+    version: pkg.version,
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "amen24",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    updates: {
+      enabled: true,
+      checkAutomatically: "ON_LOAD",
+      fallbackToCacheTimeout: 0
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "yeshua.emadunan.amen24"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "yeshua.emadunan.amen24",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#F4EFE6"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#F4EFE6",
       },
-      "package": "yeshua.emadunan.amen24"
+      package: "yeshua.emadunan.amen24"
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
     },
-    "plugins": [
-      "expo-router",
+    plugins: [
+      expo-router,
       [
-        "expo-splash-screen",
+        expo-splash-screen,
         {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#F4EFE6"
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#F4EFE6"
         }
       ],
       [
-        "expo-sqlite",
+        expo-sqlite,
         {
-          "enableFTS": true,
-          "useSQLCipher": true,
-          "android": {
-            "enableFTS": false,
-            "useSQLCipher": false
+          enableFTS: true,
+          useSQLCipher: true,
+          android: {
+            enableFTS: false,
+            useSQLCipher: false
           },
-          "ios": {
-            "customBuildFlags": [
+          ios: {
+            customBuildFlags: [
               "-DSQLITE_ENABLE_DBSTAT_VTAB=1 -DSQLITE_ENABLE_SNAPSHOT=1"
             ]
           }
@@ -62,15 +63,15 @@ export default {
       "expo-localization",
       "expo-secure-store"
     ],
-    "experiments": {
-      "typedRoutes": true
+    experiments: {
+      typedRoutes: true
     },
-    "extra": {
-      "router": {
-        "origin": false
+    extra: {
+      router: {
+        origin: false
       },
-      "eas": {
-        "projectId": "c197183c-5985-4300-a147-d737a73a267f"
+      eas: {
+        projectId: "c197183c-5985-4300-a147-d737a73a267f"
       },
       API_URL: process.env.API_URL,
       BASE_URL: process.env.BASE_URL
